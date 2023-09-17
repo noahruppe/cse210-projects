38362852cadb1b9cb3dd3791c3d1d2de26ec8db1
noahruppe <noah.ruppe30@gmail.com>
2023-09-17 13:51:53 -0400

    number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomizer = new Random();
+        int number = randomizer.Next(1,100);
+
+        Console.Write("What is your guess? ");
+        string answer1 = Console.ReadLine();
+        int number1 = int.Parse(answer1);
+        
+        while (number1 != number)
+        {
+            if (number1 > number)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("guess again ");
+                answer1= Console.ReadLine();
+                number1 = int.Parse(answer1);
+            }
+            else if (number1 < number)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("guess again ");
+                answer1= Console.ReadLine();
+                number1 = int.Parse(answer1);
+            }
+        }
+            Console.WriteLine("you got it");
+            
     }
 }
