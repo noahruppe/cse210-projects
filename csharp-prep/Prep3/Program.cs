fe5a2d6a31b9dacfffac57ec3aa5ebf112f4d0df
noahruppe <noah.ruppe30@gmail.com>
2023-09-17 14:17:55 -0400

    Changes to the number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,31 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomizer = new Random();
-        int number = randomizer.Next(1,100);
 
-        Console.Write("What is your guess? ");
-        string answer1 = Console.ReadLine();
-        int number1 = int.Parse(answer1);
-        
-        while (number1 != number)
-        {
-            if (number1 > number)
+         string answer2="";
+         while (answer2 != "no")
+         {
+         
+
+
+            Random randomizer = new Random();
+            int number = randomizer.Next(1,100);
+
+            Console.Write("What is your guess? ");
+            string answer1 = Console.ReadLine();
+            int number1 = int.Parse(answer1);
+
+            int count = 0;
+            
+            while (number1 != number)
             {
-                Console.WriteLine("Lower");
-                Console.Write("guess again ");
-                answer1= Console.ReadLine();
-                number1 = int.Parse(answer1);
+                count ++;
+                if (number1 > number)
+                {
+                    Console.WriteLine("Lower");
+                    Console.Write("guess again ");
+                    answer1= Console.ReadLine();
+                    number1 = int.Parse(answer1);
+                }
+                else if (number1 < number)
+                {
+                    Console.WriteLine("Higher");
+                    Console.Write("guess again ");
+                    answer1= Console.ReadLine();
+                    number1 = int.Parse(answer1);
+                }
             }
-            else if (number1 < number)
-            {
-                Console.WriteLine("Higher");
-                Console.Write("guess again ");
-                answer1= Console.ReadLine();
-                number1 = int.Parse(answer1);
-            }
-        }
-            Console.WriteLine("you got it");
+                Console.WriteLine("you got it");
+                Console.WriteLine($"the number of guesses: {count+1}");
+
+                Console.Write("Would you like to play again? ");
+                answer2 = Console.ReadLine();
+         }
             
     }
 }
+
+
