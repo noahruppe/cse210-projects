6a24b1b131dcdac60514867f8ecdfb6e42ac24b4
noahruppe <noah.ruppe30@gmail.com>
2023-12-07 12:41:23 -0500

    this

--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -11,7 +11,7 @@ public class Swimming : Activity
 
     public override double GetDistance()
     {
-        // Distance in miles using the provided formula
+        
         return laps * 50 / 1000.0 * 0.62;
     }
 
@@ -22,14 +22,14 @@ public class Swimming : Activity
 
     public override double GetPace()
     {
-        // Handle the case where distance is zero
+        
         if (GetDistance() > 0)
         {
             return length / GetDistance();
         }
         else
         {
-            return 0; // or any other appropriate value
+            return 0;
         }
     }
 
